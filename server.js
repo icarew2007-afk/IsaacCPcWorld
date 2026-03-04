0fa4a8ce6d901b2ac3f4ed618fe086fee20749e3
Isaac Carew <isaac@Mac.vodafone.ultrahub>
2026-03-04 18:16:48 +0000

    Another checl

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 'use strict';
 
 import express from 'express';
+import routes from "./routes.js";
+import logger from "./utils/logger.js";
+import { create } from 'express-handlebars';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => response.send('Welcome to the Playlist app!'));
-app.get('/dashboard', (request, response) => response.send('Playlist App Dashboard'));
+app.use(express.static("public"));
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
+const handlebars = create({extname: '.hbs'});
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
+
+app.use("/", routes);
+
+app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
